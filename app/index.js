3d4f7aa19c7bd6ccd2eea61ecebf190c12cb7453
Dennis Hernández <dennishernandezvargas@gmail.com>
2017-05-20 01:42:11 -0600

    adding yesno question

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import Vue from 'Vue';
+import _ from 'lodash';
+import axios from 'axios';
 
 var app = new Vue({
   el: '#app',
@@ -65,3 +67,37 @@ var app7 = new Vue({
     ]
   }
 });
+
+var watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  watch: {
+    question: function (newQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.getAnswer()
+    }
+  },
+  methods: {
+    getAnswer: _.debounce(
+      function () {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = 'Error! Could not reach the API. ' + error
+          })
+      },
+      500
+    )
+  }
+})
